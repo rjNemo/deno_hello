97506731e107e68d812b8f1db1cea38d6dccbbb5
Ruidy <r.nemausat@empfohlen.de>
2021-02-19 18:06:10 +0100

    enhance server

--- deps.ts
+++ deps.ts
@@ -4,4 +4,7 @@ export {
   assertThrows,
 } from "https://deno.land/std@0.87.0/testing/asserts.ts";
 export { readLines } from "https://deno.land/std@0.87.0/io/bufio.ts";
-export { listenAndServe } from "https://deno.land/std@0.87.0/http/server.ts";
+export {
+  listenAndServe,
+  ServerRequest,
+} from "https://deno.land/std@0.87.0/http/server.ts";
--- server.ts
+++ server.ts
@@ -1,8 +1,32 @@
-import { listenAndServe } from "./deps.ts";
+import { listenAndServe, ServerRequest } from "./deps.ts";
+import { ID, sayHello, sayRandomHello } from "./src/index.ts";
 
 const port = 8000;
 
-console.log(`Server listening on http://localhost:${port}/`);
-await listenAndServe({ port }, (req) => req.respond({ body: "Hello" }));
+const parseURL = (url: string) => {
+  const parts = url.slice(1).split("/");
+  return parts[0];
+};
 
-// write handler using req.url
+const serve = async (port: number) => {
+  console.log(`Server listening on http://localhost:${port}/`);
+
+  await listenAndServe({ port }, (req: ServerRequest) => {
+    console.log(`Request on address '${req.url}'`);
+
+    if (parseURL(req.url) === "") {
+      return req.respond({ body: sayRandomHello() });
+    }
+
+    try {
+      const value = parseInt(parseURL(req.url), 10);
+      const id = new ID(value);
+      return req.respond({ body: sayHello(id) });
+    } catch (error) {
+      console.error(error);
+      return req.respond({ body: error.message });
+    }
+  });
+};
+
+await serve(port);
